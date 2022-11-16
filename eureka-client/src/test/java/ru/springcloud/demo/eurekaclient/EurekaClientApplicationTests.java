e5a4b4fb940cfdaa9ee73496efb5e1de9d60212e
pashtetiks <Pavel-pochinka@yandex.ru>
2022-11-16 18:26:16 +0300

    Доработанный проект для клиента eureka с добавленным restful сервером

---
+++ eureka-client/src/test/java/ru/springcloud/demo/eurekaclient/EurekaClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.springcloud.demo.eurekaclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
